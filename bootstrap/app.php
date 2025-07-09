c5a49751fe4ddedfa9fad077cc2514b0df9992bd
Ahmed Mahfouz <ahmed98mahfouz@gmail.com>
2025-07-09 06:56:43 +0300

    fix auth api

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,11 +6,12 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
+        api: __DIR__ . '/../routes/api.php',
         health: '/up',
     )
-        ->withProviders([
+    ->withProviders([
         App\Providers\EventServiceProvider::class,
     ])
     ->withMiddleware(function (Middleware $middleware): void {
