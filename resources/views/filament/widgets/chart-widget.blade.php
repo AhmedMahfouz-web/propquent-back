8b093fcaa54b24f99986eb0a2b6748f7501bcdef
Ahmed Mahfouz <ahmed98mahfouz@gmail.com>
2025-09-16 19:01:57 +0300

    cashflow

--- resources/views/filament/widgets/chart-widget.blade.php
+++ resources/views/filament/widgets/chart-widget.blade.php
@@ -1,26 +1,26 @@
 <x-filament-widgets::widget class="fi-wi-chart">
     <div style="overflow-x: auto; overflow-y: hidden; padding-bottom: 20px;">
-        <div style="min-width: 1200px; height: {{ $this->getMaxHeight() }};" id="chart-container-{{ $this->getId() }}">
-            <canvas id="chart-{{ $this->getId() }}"></canvas>
+        <div style="min-width: 1200px; height: {{ $this->getMaxHeight() }};">
+            <canvas
+                x-data="{
+                    chart: null,
+                    
+                    init() {
+                        this.chart = new Chart($el, {
+                            type: '{{ $this->getType() }}',
+                            data: {{ \Illuminate\Support\Js::from($this->getData()) }},
+                            options: {{ \Illuminate\Support\Js::from($this->getOptions()) }},
+                        });
+                        
+                        $wire.on('updateChartData', ({ data }) => {
+                            this.chart.data = data;
+                            this.chart.update();
+                        });
+                    }
+                }"
+                x-init="init()"
+                wire:ignore
+            ></canvas>
         </div>
     </div>
-
-    <script>
-        document.addEventListener('DOMContentLoaded', () => {
-            const chartContainer = document.getElementById('chart-container-{{ $this->getId() }}');
-            const canvas = document.getElementById('chart-{{ $this->getId() }}');
-            if (!canvas) return;
-
-            const chart = new Chart(canvas, {
-                type: '{{ $this->getType() }}',
-                data: {{ Illuminate\Support\Js::from($this->getData()) }},
-                options: {{ Illuminate\Support\Js::from($this->getOptions()) }},
-            });
-
-            @this.on('updateChartData', ({ data }) => {
-                chart.data = data;
-                chart.update();
-            });
-        });
-    </script>
 </x-filament-widgets::widget>
