90c038635f50e45e213efc206ec49b3e9f6c50a6
Ahmed Mahfouz <ahmed98mahfouz@gmail.com>
2025-09-16 18:29:10 +0300

    cashflow

--- resources/views/filament/widgets/chart-widget.blade.php
+++ resources/views/filament/widgets/chart-widget.blade.php
@@ -1,7 +1,8 @@
 <x-filament-widgets::widget class="fi-wi-chart">
     <div style="overflow-x: auto; overflow-y: hidden; padding-bottom: 20px;">
         <div style="min-width: 1200px; height: {{ $this->getMaxHeight() }};">
-            <canvas
+            <div
+                wire:ignore
                 x-data="{
                     chart: null,
                     init: function () {
@@ -10,22 +11,19 @@
                             chart.data = data;
                             chart.update('resize');
                         });
-                        $wire.on('filterChartData', ({ data }) => {
-                            chart.data = data;
-                            chart.update('resize');
-                        });
                     },
                     createChart: function () {
-                        return this.chart = new Chart($el, {
+                        return this.chart = new Chart(this.$refs.canvas, {
                             type: '{{ $this->getType() }}',
                             data: {{ \Illuminate\Support\Js::from($this->getData()) }},
                             options: {{ \Illuminate\Support\Js::from($this->getOptions()) }},
                         });
                     },
                 }"
-                wire:ignore
                 x-init="init()"
-            ></canvas>
+            >
+                <canvas x-ref="canvas"></canvas>
+            </div>
         </div>
     </div>
 </x-filament-widgets::widget>
