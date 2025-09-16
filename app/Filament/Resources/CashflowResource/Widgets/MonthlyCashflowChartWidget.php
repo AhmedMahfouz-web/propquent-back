262d25aa5f02aeecb9296960e87760e2d42c115e
Ahmed Mahfouz <ahmed98mahfouz@gmail.com>
2025-09-16 21:56:58 +0300

    cashflow

--- app/Filament/Resources/CashflowResource/Widgets/MonthlyCashflowChartWidget.php
+++ app/Filament/Resources/CashflowResource/Widgets/MonthlyCashflowChartWidget.php
@@ -43,7 +43,7 @@ class MonthlyCashflowChartWidget extends ChartWidget
                     'borderColor' => 'rgba(34, 197, 94, 1)',
                     'borderWidth' => 3,
                     'fill' => true,
-                    'tension' => 0.3,
+                    'tension' => 1.5,
                     'pointRadius' => 4,
                     'pointHoverRadius' => 6,
                 ],
