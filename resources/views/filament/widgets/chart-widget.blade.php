1256f9e0532ca6705d0c5897f5ee5924f64290a0
Ahmed Mahfouz <ahmed98mahfouz@gmail.com>
2025-09-16 17:26:15 +0300

    cashflow

--- resources/views/filament/widgets/chart-widget.blade.php
+++ resources/views/filament/widgets/chart-widget.blade.php
@@ -1,4 +1,4 @@
-<div class="fi-wi-chart relative" style="height: 600px;">
+<div class="fi-wi-chart relative" style="height: 900px;">
     <canvas ax-load
         ax-load-src="{{ \Filament\Support\Facades\FilamentAsset::getAlpineComponentSrc('chart', 'filament/widgets') }}"
         x-data="chartWidget({
