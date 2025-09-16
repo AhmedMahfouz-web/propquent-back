151519b76286f9c2656d941424378f44452574b7
Ahmed Mahfouz <ahmed98mahfouz@gmail.com>
2025-09-16 17:25:12 +0300

    cashflow

--- resources/views/filament/widgets/chart-widget.blade.php
+++ resources/views/filament/widgets/chart-widget.blade.php
@@ -1,13 +1,9 @@
 <div class="fi-wi-chart relative" style="height: 600px;">
-    <canvas
-        ax-load
+    <canvas ax-load
         ax-load-src="{{ \Filament\Support\Facades\FilamentAsset::getAlpineComponentSrc('chart', 'filament/widgets') }}"
         x-data="chartWidget({
             cachedData: @js($this->getCachedData()),
             options: @js($this->getOptions()),
             type: @js($this->getType()),
-        })"
-        x-ignore
-        style="height: 600px !important; min-height: 600px !important;"
-    ></canvas>
+        })" x-ignore style="height: 900px !important; min-height: 600px !important;"></canvas>
 </div>
