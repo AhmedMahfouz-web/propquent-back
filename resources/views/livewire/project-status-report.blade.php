60d2e130a665014af7149f0ecf7fef671b208475
Ahmed Mahfouz <ahmed98mahfouz@gmail.com>
2025-10-12 13:23:02 +0300

    project status

--- resources/views/livewire/project-status-report.blade.php
+++ resources/views/livewire/project-status-report.blade.php
@@ -915,7 +915,7 @@
                                             <div class="expanded-content-wrapper">
                                                 <div class="content-row">
                                                     <textarea wire:model.lazy="project_notes.{{ $project->id }}" wire:key="notes-{{ $project->id }}"
-                                                        placeholder="Add notes for this project..." style="width: auto; height: 50px"
+                                                        placeholder="Add notes for this project..." style="width: auto; height: 30px"
                                                         class="w-full p-2 text-sm border border-gray-300 dark:border-gray-600 rounded-md
                                                                bg-white dark:bg-gray-700 text-gray-900 dark:text-gray-100
                                                                focus:border-blue-500 focus:ring-1 focus:ring-blue-500
