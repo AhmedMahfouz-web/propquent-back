662cf246f14878569eb56833450d45478305836d
Ahmed Mahfouz <ahmed98mahfouz@gmail.com>
2025-10-13 13:17:23 +0300

    financial report

--- app/Filament/Resources/ProjectTransactionResource.php
+++ app/Filament/Resources/ProjectTransactionResource.php
@@ -249,7 +249,7 @@ class ProjectTransactionResource extends Resource
             ])
             ->filters([
                 Tables\Filters\SelectFilter::make('project')
-                    ->relationship('project', 'title' . ' ' . 'key')
+                    ->relationship('project', 'title' . ' ' . 'project_key')
                     ->searchable()
                     ->preload(),
 
