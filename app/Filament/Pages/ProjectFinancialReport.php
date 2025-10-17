6178f5e2b44e6cc871aa7e4621935db2b18ca826
Ahmed Mahfouz <ahmed98mahfouz@gmail.com>
2025-10-18 02:27:39 +0300

    evaluation asset

--- app/Filament/Pages/ProjectFinancialReport.php
+++ app/Filament/Pages/ProjectFinancialReport.php
@@ -372,6 +372,10 @@ class ProjectFinancialReport extends Page implements HasForms
                 $calculatedValue = $previousAssetEvaluation + $monthData['expense_asset'] - $monthData['revenue_asset'] + $monthData['value_correction'];
                 $monthData['evaluation_asset'] = $calculatedValue;
                 
+                // Debug for Q1 Villa - simple alert
+                if (strpos($project->title, 'Q1 Villa') !== false && $month === '2025-10-01') {
+                    $monthData['debug_oct'] = "Oct Calc: Prev={$previousAssetEvaluation} + Exp={$monthData['expense_asset']} - Rev={$monthData['revenue_asset']} + Corr={$monthData['value_correction']} = {$calculatedValue}";
+                }
                 
                 // Update previous evaluation for next iteration
                 $previousAssetEvaluation = $calculatedValue;
--- resources/views/filament/pages/project-financial-report.blade.php
+++ resources/views/filament/pages/project-financial-report.blade.php
@@ -131,6 +131,9 @@
                                             @elseif ($key === 'evaluation_asset')
                                                 <span class="font-medium text-xs text-gray-700 dark:text-gray-300">
                                                     ${{ number_format($projectData['months'][$month][$key] ?? 0, 2) }}
+                                                    @if(isset($projectData['months'][$month]['debug_oct']))
+                                                        <br><small class="text-red-600 text-xs">{{ $projectData['months'][$month]['debug_oct'] }}</small>
+                                                    @endif
                                                 </span>
                                             @else
                                                 <span class="text-xs text-gray-700 dark:text-gray-300">
