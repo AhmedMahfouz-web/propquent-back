cc45c02c52347ea3a4e4629203033cdecebf1bcd
Ahmed Mahfouz <ahmed98mahfouz@gmail.com>
2025-10-17 22:36:12 +0300

    asset evaluation

--- resources/views/filament/pages/project-financial-report.blade.php
+++ resources/views/filament/pages/project-financial-report.blade.php
@@ -131,7 +131,7 @@
                                             @elseif ($key === 'evaluation_asset')
                                                 <span class="font-medium text-xs text-gray-700 dark:text-gray-300">
                                                     ${{ number_format($projectData['months'][$month][$key] ?? 0, 2) }}
-                                                    @if(isset($projectData['months'][$month]['debug_info']))
+                                                    @if(isset($projectData['months'][$month]) && array_key_exists('debug_info', $projectData['months'][$month]))
                                                         <br><small class="text-red-500">{{ $projectData['months'][$month]['debug_info'] }}</small>
                                                     @endif
                                                 </span>
